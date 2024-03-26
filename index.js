c3b4ff3a828c56d5fc8ca8527cb9420ca3be799f
Edgar <edgarasige@gmail.com>
2024-03-26 20:07:47 +0300

    Comleted assingments

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function inputSum(accumulator, value) {
+    return accumulator + value;
+  }
+  
+  const totalBatteries = batteryBatches.reduce(inputSum, 0);
+  
+  console.log(totalBatteries)
